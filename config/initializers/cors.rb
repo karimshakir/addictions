710fdd7d664e8b1fa4f090e93dda39a6267b3108
“karimshakir” <karimshakir99@gmail.com>
2018-12-15 13:03:52 -0600

    adds https://www.capstone-addict.com to cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,8 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     origins 'localhost:8080'
+    origins 'https://www.capstone-addict.com'
+
 
     resource '*',
       headers: :any,
