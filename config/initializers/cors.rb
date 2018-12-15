f72d296a44f300bfd5715741e7e6202a57af4bce
“karimshakir” <karimshakir99@gmail.com>
2018-12-15 16:20:42 -0600

    update to cors syntax

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,9 +1,13 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     origins 'localhost:8080'
+    resource '*',
+      headers: :any,
+      methods: [:get, :post, :put, :patch, :delete, :options, :head]
+  end
+
+    allow do
     origins 'https://www.capstone-addict.com'
-
-
     resource '*',
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head]
